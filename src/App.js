b9f67f648afa20b80ca7299c9576c0081e2f4fa8
chih shao <sam125055869@gmail.com>
2022-03-20 19:38:43 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import './App.css';
-import Nav from './components/Nav';
-import Products from './pages/Products';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Nav from "./components/Nav";
+import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
+import Article from "./pages/Article";
+import ArticleDetail from "./pages/ArticleDetail";
 
 function App() {
-  
   return (
     <BrowserRouter>
-      <Nav/>
+      <Nav />
       <Routes>
         <Route path="products" element={<Products />} />
         <Route path="products/:productId" element={<ProductDetail />} />
-        <Route path="article" />
-        <Route path="article/:articleId" />
+        <Route path="article" element={<Article />} />
+        <Route path="article/:articleId" element={<ArticleDetail />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
 
 export default App;
